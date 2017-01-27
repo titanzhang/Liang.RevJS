7f96a06f9816efbcf09a54946f6a1f70d8f6c85d
Liang Zhang <titanzhang@gmail.com>
2017-01-26 18:49:08 -0800

    add log

--- batch.sh
+++ batch.sh
@@ -26,7 +26,7 @@ start() {
 		echo "Process already running:$SERVER_PID"
 	else
 		echo "Starting ..."
-		$CMD > /dev/null &
+		$CMD >> batch.log &
 		SERVER_PID=$!
 		let "SERVER_PID = $SERVER_PID + 0"
 		sleep 1
