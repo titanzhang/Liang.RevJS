853c75309e4552368e1ca28e1f31ef44418fe5f4
Liang Zhang <titanzhang@gmail.com>
2017-02-08 23:24:25 -0800

    add batch debug

--- batch/batch.js
+++ batch/batch.js
@@ -1,11 +1,12 @@
 require('./base.js');
-var scheduler = load('batch.scheduler');
 
-// month, date, hour, minute
-scheduler.register('*', '*', '4', '5', 'UpdatePrice', load('batch.job.UpdatePrice'));
+if (process.argv[2] === 'debug') {
+	setTimeout(load('batch.job.UpdatePrice'), 0);
+} else {
+	var scheduler = load('batch.scheduler');
 
-// scheduler.register('*', '*', '*', '*', 'test', function() {
-// 	console.log('test');
-// });
-
-scheduler.run();
+	// month, date, hour, minute
+	scheduler.register('*', '*', '4', '5', 'UpdatePrice', load('batch.job.UpdatePrice'));
+	
+	scheduler.run();
+}
