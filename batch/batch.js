a9f13f274cb175d5690de9da5d46e4d04d6945f4
Liang Zhang <titanzhang@gmail.com>
2017-03-31 20:32:44 -0700

    Change job schedule

--- batch/batch.js
+++ batch/batch.js
@@ -4,7 +4,7 @@ var scheduler = load('batch.scheduler');
 
 // month, date, hour, minute
 scheduler.register('*', '*', '4', '5', 'UpdatePrice', load('batch.job.UpdatePrice'));
-scheduler.register('*', '1,15', '4', '5', 'UpdateAll', load('batch.job.UpdateAll'));
+scheduler.register('*', '1,15', '0', '5', 'UpdateAll', load('batch.job.UpdateAll'));
 
 if (process.argv[2] === 'debug' && process.argv[3] !== undefined) {
 	scheduler.execNow(process.argv[3]);
